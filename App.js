17344dec9d5e4325434c3c50839eb359dbd96009
Anakimberly <22620053@tlaxiaco.tecnm.mx>
2025-09-23 00:41:10 -0600

    en la lista alumnos se agregaron diferentes alumnos y de igual manera se le coloco diferentes colores y diseños

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 //import BotonCambio from './scr/Boton/Botoncambio';
 //import Home from './scr/Pantalladeinicio/Home';
-//import Homeprincipal from './scr/Pantalladeinicio/Homeprincipal';
 import { Listadealumnos } from './scr/Listadealumnos/Listadealumnos';
 export default function App() {
 
   return (
     //<BotonCambio/>
     //<Home/>
-    //<Homeprincipal/>
     <Listadealumnos />
    ); 
 }
--- scr/Listadealumnos/Listadealumnos.js
+++ scr/Listadealumnos/Listadealumnos.js
@@ -7,28 +7,95 @@ export const Listadealumnos = () => {
         <SafeAreaView style={style.mainS}>
             {/* inicio de AppBar */}
             <View style={style.appBar}>
-                <Ionicons name="arrow-back" size={30} color={'#000000ff'} />
-                <Text style={style.appBarTitle}>Lista de Usuarios</Text>
+                <Ionicons name="arrow-back" size={30} color={'#ffffffff'} />
+                <Text style={style.appBarTitle}>ALUMNOS REGISTRADOS</Text>
                 <View style={{width:20 }}></View>
                 </View>
             {/* fin de AppBar */}
             {/* Inicio de contenido (lista de alumnos)*/}
-            <ScrollView style={{padding:16}}>
-                <Text style={style.sectionTilte}>Alumnos de Aplicaciones Moviles </Text>
-                {[1,2,3,4,5,6,7].map((item, index) => (
-                    <View style={style.card}>
-                 <Image source={{ uri: 'https://i.pinimg.com/474x/6d/5e/38/6d5e38d19bf4c0c9554b1e6beab75952.jpg'}} style={style.avatar} />
-                <View style={style.cardinfo}>
-                    <Text style={style.userName}>Ana Kimberly</Text>
-                 <Text style={style.userDetails}>Ing. Sistemas Computacionales</Text>
-                 <TouchableOpacity style={style.saveButton}>
-                    <Text style={style.saveButtonText}> Ver mas</Text>
+            <ScrollView style={{padding:16}}>        
+                  <ScrollView style={{padding:16}}>        
+    {/* Alumno 1 */}
+    <View style={style.card}>
+        <Image source={{ uri: 'https://w7.pngwing.com/pngs/298/171/png-transparent-avatar-face-girl-female-woman-profile-happy-avatar-icon.png'}} style={style.avatar} />
+        <View style={style.cardinfo}>
+            <Text style={style.userName}>Ana Kimberly</Text>
+            <Text style={style.userDetails}>Ing. Sistemas Computacionales</Text>
+            <Text style={style.userDetails}>22620053</Text>
+            <Text style={style.userDetails}>7BS</Text>
+           
+        </View>
+    </View>
 
-                 </TouchableOpacity>
-                </View>
-                </View>
+    {/* Alumno 2 */}
+    <View style={style.card}>
+        <Image source={{ uri: 'https://i0.wp.com/www.mascarilla.org/wp-content/uploads/2020/06/C%C3%B3mo-Crear-mi-Avatar-de-Facebook-Te-Explicamos-Paso-a-Paso-2.jpg?fit=253%2C283&ssl=1'}} style={style.avatar} />
+        <View style={style.cardinfo}>
+            <Text style={style.userName}>Brenda Silva</Text>
+            <Text style={style.userDetails}>Lic. Administracion</Text>
+            <Text style={style.userDetails}>22620053</Text>
+            <Text style={style.userDetails}>7BA</Text>
+      
+        </View>
+    </View>
+     {/* Alumno 3 */}
+    <View style={style.card}>
+        <Image source={{ uri: 'https://static.vecteezy.com/system/resources/thumbnails/027/951/137/small_2x/stylish-spectacles-guy-3d-avatar-character-illustrations-png.png'}} style={style.avatar} />
+        <View style={style.cardinfo}>
+            <Text style={style.userName}>Raul Hernandez</Text>
+            <Text style={style.userDetails}>Lic.Arquitectura</Text>
+            <Text style={style.userDetails}>22620053</Text>
+            <Text style={style.userDetails}>7BA</Text>
+      
+        </View>
+    </View>
+     {/* Alumno 4 */}
+    <View style={style.card}>
+        <Image source={{ uri: 'https://www.informador.mx/__export/1591209620028/sites/elinformador/img/2020/06/03/whatsapp_image_2020-06-03_at_1_22_36_pm_x1x_crop1591209586178.jpg_423682103.jpg'}} style={style.avatar} />
+        <View style={style.cardinfo}>
+            <Text style={style.userName}>Dalila Fernandez</Text>
+            <Text style={style.userDetails}>Ing. Gestion Empresarial</Text>
+            <Text style={style.userDetails}>22620053</Text>
+            <Text style={style.userDetails}>7BG</Text>
+      
+        </View>
+    </View>
+     {/* Alumno 5 */}
+    <View style={style.card}>
+        <Image source={{ uri: 'https://i.pinimg.com/236x/56/77/62/5677627c338956d1cb9bbdb7f49ae79e.jpg'}} style={style.avatar} />
+        <View style={style.cardinfo}>
+            <Text style={style.userName}>Romina Santiago</Text>
+            <Text style={style.userDetails}>Ing. Mecatronica</Text>
+            <Text style={style.userDetails}>22620053</Text>
+            <Text style={style.userDetails}>7UM</Text>
+      
+        </View>
+    </View>
+     {/* Alumno 6 */}
+    <View style={style.card}>
+        <Image source={{ uri: 'https://i.pinimg.com/236x/e9/57/2a/e9572a70726980ed5445c02e1058760b.jpg'}} style={style.avatar} />
+        <View style={style.cardinfo}>
+            <Text style={style.userName}>Sergio Gomez</Text>
+            <Text style={style.userDetails}>Ing. Civil</Text>
+            <Text style={style.userDetails}>22620053</Text>
+            <Text style={style.userDetails}>7BA</Text>
+      
+        </View>
+    </View>
+     {/* Alumno 7 */}
+    <View style={style.card}>
+        <Image source={{ uri: 'https://c0.klipartz.com/pngpicture/517/749/gratis-png-equipo-iconos-usuario-perfil-avatar-mujer-joven-thumbnail.png'}} style={style.avatar} />
+        <View style={style.cardinfo}>
+            <Text style={style.userName}>Ameli Reyes</Text>
+            <Text style={style.userDetails}>Ing. Sistemas Computacionales</Text>
+            <Text style={style.userDetails}>22620053</Text>
+            <Text style={style.userDetails}>7BS</Text>
+      
+        </View>
+    </View>
+</ScrollView>
 
-                ))}
+            
 
             </ScrollView>
             {/* cierre de contenido (lista de alumnos)*/}
@@ -55,14 +122,14 @@ export const Listadealumnos = () => {
 const style = StyleSheet.create({ 
  mainS:{
         flex:1,
-        backgroundColor:'#b4aee9ff',
+        backgroundColor:'#b27f53ff',
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight:44,
         // padding: 50
     },
     appBar:{
         height:50,
         width:'100%',
-        backgroundColor:'#ffffffff',
+        backgroundColor:'#e7c5a1ff',
         flexDirection:'row',
         alignItems:'center',
          justifyContent:'space-between',
@@ -84,11 +151,13 @@ const style = StyleSheet.create({
     card:{
         padding:16,
         flexDirection:'row',
-        backgroundColor:'#ffffffff',
+        backgroundColor:'#e2bea5ff',
        borderRadius:16,
        elevation:2,
          marginBottom:5,
     },
+    
+     
     avatar:{
          width:60 ,
       height : 60,
@@ -99,6 +168,11 @@ const style = StyleSheet.create({
     cardinfo:{
         flex:1,
         justifyContent:'center',
+    },
+    cardinfo2:{
+        flex:1,
+        justifyContent:'center',
+        marginBottom:10,
     },
     userName:{
         fontSize: 16,
@@ -130,7 +204,7 @@ const style = StyleSheet.create({
         justifyContent:'space-around',
         alignItems:'center',
         height:70,
-        backgroundColor:'#ffffffff',
+        backgroundColor:'#dec2a1ff',
         borderTopWidth:1,
         // marginBottom:10,
     },
