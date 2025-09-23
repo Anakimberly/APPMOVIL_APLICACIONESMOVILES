9929c9bd6f35d492c62ee003512c080bf1b468eb
Anakimberly <22620053@tlaxiaco.tecnm.mx>
2025-09-22 21:05:54 -0600

    se descomento una linea

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 //import BotonCambio from './scr/Boton/Botoncambio';
-//import Home from './scr/Pantalladeinicio/Home';
-import Homeprincipal from './scr/Pantalladeinicio/Homeprincipal';
+import Home from './scr/Pantalladeinicio/Home';
+//import Homeprincipal from './scr/Pantalladeinicio/Homeprincipal';
 export default function App() {
 
   return (
     //<BotonCambio/>
-    //<Home/>
-    <Homeprincipal/>
+    <Home/>
+    //<Homeprincipal/>
    ); 
 }
